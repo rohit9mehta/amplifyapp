6ef3032750e5360821f4dc1c2481b1c47dcb1ab5
rohit9mehta <51008282+rohit9mehta@users.noreply.github.com>
2019-09-11 22:20:49 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deploying and hosting a React app with AWS! Link to learn React: <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
